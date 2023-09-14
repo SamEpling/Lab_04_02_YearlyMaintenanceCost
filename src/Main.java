a10beed2fa5885ebe4b8ec30b54c8cc6471f229a
Sam Epling <sgepling@icloud.com>
2023-09-14 15:13:32 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double winterCost = 1500.95;
+        double springCost = 2100.35;
+        double summerCost = 2520.24;
+        double fallCost = 3500.25;
+        double totalCost = winterCost + springCost + summerCost + fallCost;
+        System.out.println("The cost for Winter is " + winterCost + ", and the cost for Spring is " + springCost + ", and the cost for Summer is "
+                + summerCost + ", and the cost for Fall is " + fallCost + ", so the total cost is " + totalCost);
+
+    }
+}
